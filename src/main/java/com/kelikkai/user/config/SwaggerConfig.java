6c552a6a29116c68747f9558f3520a4480b8fd85
APAC\ADH2COB <ajaykumar.dhandapani@in.bosch.com>
2017-09-15 16:05:01 +0530

    Modified pom for swagger

--- src/main/java/com/kelikkai/user/config/SwaggerConfig.java
+++ src/main/java/com/kelikkai/user/config/SwaggerConfig.java
@@ -6,48 +6,46 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class SwaggerConfig.
+ * @author PDN7KOR
+ *
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig
-{
-
-	/** The swagger flag. */
+public class SwaggerConfig {  
+	
 	@Value("${swagger.flag}")
 	private String swaggerFlag;
-
-	/**
-	 * Api.
-	 *
-	 * @return the docket
-	 */
-	@Bean
-	public Docket api()
-	{
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
-	}
-
-	/**
-	 * Api info.
-	 *
-	 * @return the api info
-	 */
-	private ApiInfo apiInfo()
-	{
-		return new ApiInfo("REST APIs for Kelikkai User service",
-				"This WebUI describe the REST APIs that can be used to manipulate the 'User' service", "v2.0",
-				"Terms of service", "", "License of API", "API license URL");
-	}
+	
+    /**
+     * @return
+     */
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+        .groupName("User")
+        .apiInfo(apiInfo()).enable(Boolean.valueOf(true))
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.kelikkai.user.controller"))
+        .paths(regex("/.*"))
+        .build();                                           
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+          "REST APIs for the Kelikkai User service",
+          "This WebUI describe the REST APIs that can be used to manipulate the 'User' service",
+          "v2.0",
+          "Terms of service",
+          new Contact("Ajay Kumar", "", "ajaytechworkshop@gmail.com"),
+          "License of API",
+          "API license URL");
+    }
 }
